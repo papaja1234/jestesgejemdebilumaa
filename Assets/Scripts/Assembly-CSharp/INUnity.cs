7fc51625870d5c3a2551349e980032c132a312e0
Anstro Pleuton <anstro.pleuton@proton.me>
2023-07-18 22:19:10 +0530

    Weird branch management, revert before Goggs' changes

--- Assets/Scripts/Assembly-CSharp/INUnity.cs
+++ Assets/Scripts/Assembly-CSharp/INUnity.cs
@@ -38,7 +38,8 @@ public static class INUnity
 
 	static INUnity()
 	{
-		Version = Version.Parse(Application.version);
+		// BUG: Parse errors
+		// Version = Version.Parse(Application.version);
 		VersionText = Application.version;
 		DataPath = Application.persistentDataPath;
 		SettingsPath = Application.persistentDataPath + "/Settings";
