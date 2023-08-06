fb86f43253d9f93235dddfc2f80df0e89387a2b3
Anstro Pleuton <anstro.pleuton@proton.me>
2023-08-06 21:10:12 +0530

    Revert "Attempt to fix colored frame"
    This reverts commit 50f6f9a92cd8d676fe6eef1b2ee395ee64404a21.

--- Assets/Scripts/Assembly-CSharp/Frame.cs
+++ Assets/Scripts/Assembly-CSharp/Frame.cs
@@ -95,8 +95,9 @@ public class Frame : BasePart
 				{
 					// BUG: Weird colored frame
 					//     meshRenderer.material.shader = INUnity.LoadShader("Unlit_ColorTransparent_GrayOverlay");
-					// Fix: Use "PreAlpha_Unlit_ColorTransparent_Geometry"
-					meshRenderer.material.shader = INUnity.LoadShader("PreAlpha_Unlit_ColorTransparent_Geometry");
+					// Fix: Use "_Custom_PreAlpha_Unlit_ColorTransparent_Geometry"
+					// Provided by: Goggs
+					meshRenderer.material.shader = INUnity.LoadShader("_Custom_PreAlpha_Unlit_ColorTransparent_Geometry");
 				}
 				meshRenderer.material.color = clear;
 			}
