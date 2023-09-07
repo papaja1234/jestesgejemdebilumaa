3d203d6306d02c7f1fa6833f29ed48cc9f02c229
Anstro Pleuton <anstro.pleuton@proton.me>
2023-09-07 20:09:22 +0530

    Fix one thing...

--- Assets/Scripts/Assembly-CSharp/CustomizePartUI.cs
+++ Assets/Scripts/Assembly-CSharp/CustomizePartUI.cs
@@ -72,7 +72,6 @@ public class CustomizePartUI : WPFMonoBehaviour
 		}
 		cachedPartListing.CreateSelectionIcons();
 		initButtonsDone = true;
-		ExportAllPartData(WPFMonoBehaviour.gameData, @"C:\Users\Me\Documents\CustomizePartUI.InitButtons().txt");
 	}
 
 	public void CustomButtonPressed(int partTypeIndex, int partIndex)
