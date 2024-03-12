1a55676bf5fb13c2c70f1f36837e933a8dec1d6d
Goggs <the801140@outlook.com>
2024-03-12 23:04:28 +0800

    MineSweeper Texture Import

--- Assets/Scripts/Assembly-CSharp/MenuContraptionManager.cs
+++ Assets/Scripts/Assembly-CSharp/MenuContraptionManager.cs
@@ -220,9 +220,7 @@ public class MenuContraptionManager : MonoBehaviour
 		for (int i = 0; i < m_contraptionAssets.Count - 1; i++)
 		{
 			int index = Random.Range(i, m_contraptionAssets.Count);
-			TextAsset value = m_contraptionAssets[i];
-			m_contraptionAssets[i] = m_contraptionAssets[index];
-			m_contraptionAssets[index] = value;
+			(m_contraptionAssets[i], m_contraptionAssets[index]) = (m_contraptionAssets[index], m_contraptionAssets[i]);
 		}
 	}
 }
